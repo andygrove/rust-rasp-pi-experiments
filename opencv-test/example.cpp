47cebe70b3e476706a162f852d91891ce1d53eaf
Andy Grove <andygrove73@gmail.com>
2016-08-21 18:39:41 +0000

    save

--- opencv-test/example.cpp
+++ opencv-test/example.cpp
@@ -5,22 +5,50 @@
 using namespace std;
 using namespace cv;
 
-VideoCapture capture;
+VideoCapture inputVideo;
+VideoWriter outputVideo;
+
 Mat frame;
 
-extern int video_init() {
-  if (!capture.open(0)) {
+extern "C" int32_t video_init() {
+  if (!inputVideo.open(0)) {
     return -1;
   }
+
+//  int fourcc = static_cast<int>(inputVideo.get(CV_CAP_PROP_FOURCC)); 
+ // cout << "fourcc = " << fourcc << endl;
+
+  Size S = Size((int) inputVideo.get(CV_CAP_PROP_FRAME_WIDTH),    // Acquire input size
+                  (int) inputVideo.get(CV_CAP_PROP_FRAME_HEIGHT));
+
+  //outputVideo.open("out.avi", VideoWriter::fourcc('M','J','P','G'), inputVideo.get(CV_CAP_PROP_FPS), S, true);
+  outputVideo.open("out.mp4", VideoWriter::fourcc('M','P','4','V'), inputVideo.get(CV_CAP_PROP_FPS), S, true);
+  if (!outputVideo.isOpened()) {
+    cerr << "failed to open video output file" << endl;
+    return -2;
+  }
+
   return 0;
 }
 
-extern int video_grab() {
-  capture >> frame;
+extern "C" int32_t video_grab(const char *data) {
+
+  inputVideo >> frame;
+
   if (frame.empty()) {
     return -1;
   }
-  imshow("test", frame);
+
+  putText(frame, data, cvPoint(30,30), 
+    FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200,200,250), 1, CV_AA);
+
+  outputVideo << frame;
+
+  return 0;
+}
+
+extern "C" int32_t video_close() {
+  outputVideo.release();
   return 0;
 }
 
--- opencv-test/src/main.rs
+++ opencv-test/src/main.rs
@@ -1,20 +1,29 @@
 extern crate libc;
 
+use libc::c_char;
+use std::ffi::CStr;
+use std::ffi::CString;
+
 #[link(name="opencvexample")]
 extern {
   fn video_init() -> i32;
-  fn video_grab() -> i32;
+  fn video_grab(text: *const c_char) -> i32;
+  fn video_close() -> i32;
 }
 
 
 fn main() {
   let i = unsafe { video_init() };
+  println!("video_init() returned {}", i);
   if i==0 {
-    loop {
-      let j = unsafe { video_grab() };
+    for frame in 0 .. 100 {
+      let text = CString::new(format!("Frame: {}", frame)).unwrap(); 
+      let j = unsafe { video_grab(text.as_ptr()) };
       if j != 0 {
         break;
       }
     }
+    unsafe { video_close() };
+    println!("Finished");
   }
 }
