465b4af73929ae4a77fe5ceabb24790caaa85d2f
Andy Grove <andygrove73@gmail.com>
2016-08-21 12:56:02 +0000

    save

--- serial-io/src/main.rs
+++ serial-io/src/main.rs
@@ -11,23 +11,28 @@ use serial::prelude::*;
 fn main() {
     println!("Connecting...");
 
-    let mut port = serial::open("/dev/ttyAMA0").unwrap();
+    let mut port = serial::open("/dev/ttyS0").unwrap();
 
     port.reconfigure(&|settings| {
         settings.set_baud_rate(serial::Baud57600).unwrap();
         settings.set_char_size(serial::Bits8);
         settings.set_parity(serial::ParityNone);
         settings.set_stop_bits(serial::Stop1);
-        settings.set_flow_control(serial::FlowNone);
+//        settings.set_flow_control(serial::FlowSoftware);
         Ok(())
     }).unwrap();
 
-    port.set_timeout(Duration::from_millis(5000)).unwrap();
+    port.set_timeout(Duration::from_millis(1000)).unwrap();
 
-    println!("Reading...");
-    let mut buf = vec![0_u8; 50];
-    let foo = port.read(&mut buf[..]).unwrap();
+    
+    loop {
+//	println!("Reading...");
+        let mut buf = vec![0_u8; 64];
+        let n = port.read(&mut buf[..]).unwrap();
 
-    println!("{:?}", buf);
+        for i in 0..n {
+            print!("{}", buf[i] as char);
+        }
 
+    }   
 }
